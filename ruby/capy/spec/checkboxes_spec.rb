0af18044c6982cf878051cd867dd9edf486ea28c
Felipe <flp_carvalho@hotmail.com>
2020-01-31 14:57:37 -0300

    capy update

---
+++ ruby/capy/spec/checkboxes_spec.rb
@@ -0,0 +1,5 @@
+describe 'Caixa de seleção' do
+
+it
+
+end
---
+++ ruby/capy/spec/hello_spec.rb
@@ -0,0 +1,10 @@
+describe 'Meu primeiro script' do
+
+    it 'visitar a página' do
+
+        visit 'https://training-wheels-protocol.herokuapp.com/'
+        puts page.title
+
+    end
+
+end
