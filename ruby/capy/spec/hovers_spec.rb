e2c73aad108584b7e6f6b436bb9dbc8b923ddd24
flpcarvalho <flp_carvalho@hotmail.com>
2020-02-02 21:25:49 -0300

    drop

--- ruby/capy/spec/hovers_spec.rb
+++ ruby/capy/spec/hovers_spec.rb
@@ -4,21 +4,21 @@ describe "Mouse Hover", :hovers do
   end
 
   it "quando passo o mouse sobre o Blade" do
-    card = find("img[alt=Blade]")
+    card = find("img[alt*=Blade]")
     card.hover
 
     expect(page).to have_content "Nome: Blade"
   end
 
   it "quando passo o mouse sobre o Pantera Negra" do
-    card = find("img[alt='Pantera Negra']")
+    card = find("img[alt^=Pantera]")
     card.hover
 
     expect(page).to have_content "Nome: Pantera Negra"
   end
 
   it "quando passo o mouse sobre o Homem Aranha" do
-    card = find("img[alt='Homem Aranha']")
+    card = find("img[alt$=Aranha]")
     card.hover
 
     expect(page).to have_content "Nome: Homem Aranha"
