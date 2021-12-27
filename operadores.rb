c744d7724f7ccd713e01c2d9ffb6b52a8e1da4f3
Felipe Carvalho <flp_carvalho@hotmail.com>
2021-12-27 19:18:52 -0300

    operadores

--- operadores.rb
+++ operadores.rb
@@ -8,5 +8,15 @@ numero1 = gets.chomp.to_i
 puts 'Informe o número 2:'
 numero2 = gets.chomp.to_i 
 
-total = numero1 + numero2
+#total = numero1 + numero2
+#total = numero1 - numero2
+#total = numero1 * numero2
+total = numero1 / numero2
+
 puts total
+
+
+v1 = 12
+v2 = 11
+
+puts v1 > v2
