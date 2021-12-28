d01b9d5c70f9f3780826197d98995e003bd9416b
Felipe Carvalho <flp_carvalho@hotmail.com>
2021-12-28 11:33:41 -0300

    metodos

---
+++ metodos.rb
@@ -0,0 +1,5 @@
+def diga_ola(nome)
+  puts "Ola!" + nome
+end
+
+diga_ola("Felipe")
--- operadores.rb
+++ operadores.rb
@@ -1,20 +1,29 @@
-numero1 = 0
-numero2 = 0
-total = 0
+# numero1 = 0
+# numero2 = 0
+# total = 0
 
-puts 'Informe o número 1:'
-numero1 = gets.chomp.to_i
+# puts 'Informe o número 1:'
+# numero1 = gets.chomp.to_i
 
-puts 'Informe o número 2:'
-numero2 = gets.chomp.to_i 
+# puts 'Informe o número 2:'
+# numero2 = gets.chomp.to_i
 
 #total = numero1 + numero2
 #total = numero1 - numero2
 #total = numero1 * numero2
-total = numero1 / numero2
+#total = numero1 / numero2
 
-puts total
+# puts total
 
+# Operadores de Comparacao
+
+# maior que >
+# menor que <
+# maior ou igual >=
+# menor ou igual <=
+# diferente !=
+# igual a ==
+# igual eql?
 
 v1 = 12
 v2 = 11
