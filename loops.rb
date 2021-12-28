4a6066ff8948722a9746fd3971291b3c08da56b1
Felipe Carvalho <flp_carvalho@hotmail.com>
2021-12-28 11:44:24 -0300

    loops

---
+++ loops.rb
@@ -0,0 +1,9 @@
+# 5.times do |i|
+#   puts "Repetindo a mensagem " + i.to_s + " vez(es)."
+# end
+
+init = 0
+while init <= 10
+  puts "Repetindo a mensagem " + init.to_s + " vez(es). "
+  init += 1
+end
